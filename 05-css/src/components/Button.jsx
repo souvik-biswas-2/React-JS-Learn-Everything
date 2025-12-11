39b3a02ebcb2ff6a2c1dc2c85805d055bfd8c4d8
Souvik Biswas <souvik-biswas-2>
2025-12-11 16:20:00 +0530

    CSS Collision

---
+++ 05-css/src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../styles/Button.css'
+const Button = () => {
+  return (
+    <div className='btn'>Button</div>
+  )
+}
+
+export default Button
---
+++ 05-css/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <h3>Sheriyans</h3>
+        <button>Login</button>
+    </div>
+  )
+}
+
+export default Header
