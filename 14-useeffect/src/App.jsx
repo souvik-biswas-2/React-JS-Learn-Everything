95b63bb6d15706294d840f952ac8720e77cf2c57
Souvik Biswas <souvik-biswas-2>
2025-12-13 19:45:05 +0530

    useEffect Mounting

---
+++ 14-useeffect/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Mounting from './Mounting'
+
+const App = () => {
+  
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ 14-useeffect/src/Mounting.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+const Mounting = () => {
+
+    const [num, setNum] = useState(0)
+
+    useEffect(() => {
+        console.log('Use Effect is running....');
+    })
+    
+
+  return (
+    <div className='flex flex-row'>
+      <h1 className='bg-emerald-500 rounded-full m-5 px-2 py-2 inline'>{num}</h1>
+      <button onClick={() => {
+        setNum(num+1)
+      }} className='bg-emerald-500 rounded-full m-5 px-6 py-2' >Click</button>
+    </div>
+  )
+}
+
+export default Mounting
